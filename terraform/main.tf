d8453e8ba75321ae3029b69642f026c03236cb15
chrisorisawayi <christopher.orisawayi@hybridaccess.net>
2022-04-10 18:52:05 +0100

    terraform fmt

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,6 +1,6 @@
 resource "random_uuid" "uuid" {}
 
 resource "azurerm_resource_group" "rg" {
-  location            = "uksouth"
-  name                = "gha-demo"
+  location = "uksouth"
+  name     = "gha-demo"
 }
