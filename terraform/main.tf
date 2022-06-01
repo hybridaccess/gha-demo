259ed16db4dafd14c81dd45c7ff77fdecdd1d9fa
chrisorisawayi <christopher.orisawayi@hybridaccess.net>
2022-06-01 08:04:56 +0100

    update

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,3 +4,19 @@ resource "azurerm_resource_group" "rg" {
   location = "uksouth"
   name     = "gha-demo"
 }
+
+resource "null_resource" "demo" {
+
+  provisioner "local-exec" {
+    command = <<-EOT
+      ls
+      whoami
+      pwd
+    EOT
+  }
+
+
+  triggers = {
+    always_run = timestamp()
+  }
+}
