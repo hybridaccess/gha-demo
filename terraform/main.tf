bcfbbfc46c880cda5ab754b4e50d7616dd4979ea
chrisorisawayi <christopher.orisawayi@hybridaccess.net>
2022-07-02 18:26:52 +0100

    update local-exec

--- terraform/main.tf
+++ terraform/main.tf
@@ -8,7 +8,7 @@ resource "azurerm_resource_group" "rg" {
 resource "null_resource" "demo" {
 
   provisioner "local-exec" {
-    command     = "az group list -o table"
+    command     = "locate az && echo $path"
     interpreter = ["/bin/bash", "-c"]
 
   }
